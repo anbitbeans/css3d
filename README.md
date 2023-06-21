44eb560c06f99af4513c04e70e58e164e918c8f4
bb_nitta <akiyuki.nitta@bitbeans.com>
2023-06-21 12:37:13 +0900

    v1

--- README.md
+++
@@ -1,4 +0,0 @@
-# CSS 3D
-
-A Pen created on CodePen.io. Original URL: [https://codepen.io/bbfront/pen/xxydPoj](https://codepen.io/bbfront/pen/xxydPoj).
-
--- script.js
+++ script.js
@@ -15,46 +15,32 @@ animate();
 
 function init() {
 
-	const aspect = window.innerWidth / window.innerHeight;
-	//camera = new THREE.OrthographicCamera(frustumSize * aspect / - 2, frustumSize * aspect / 2, frustumSize / 2, frustumSize / - 2, 1, 1000);
-	camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-	camera.position.set(10, 10, 500);
-	// camera.rotation.set(10,60,0);
+	camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 2000);
+	camera.position.set(30, 28, 30);
 	scene = new THREE.Scene();
-	scene.background = new THREE.Color(0xf0f0f0);
-
+	scene.background = new THREE.Color(0x333333);
+	const light = new THREE.SpotLight(0xFFFFFF, 4, 50, Math.PI / 4, 10, 0.5);
+	light.position.set(0, 45, 0);
+	scene.add(light);
 	scene2 = new THREE.Scene();
 
-	const material = new THREE.MeshBasicMaterial({ color: 0x000000, wireframe: true, wireframeLinewidth: 1, side: THREE.DoubleSide });
-
-
-	const cssobj = document.getElementById("css3d")
-	const css3dobj = new CSS3DObject(cssobj)
-	css3dobj.position.set(0, 10, -4);
-	css3dobj.rotation.set(0, 0, 0);
-	css3dobj.scale.set(0.3, 0.3, 0.3);
-	scene2.add(css3dobj)
-
-	const cssobj2 = document.getElementById("css3d2")
-	const css3dobj2 = new CSS3DObject(cssobj2)
-	css3dobj2.position.set(0, 10, -4.01);
-	css3dobj2.rotation.set(0, 0, 0);
-	css3dobj2.scale.set(0.3, 0.3, 0.3);
-	scene2.add(css3dobj2)
-	//gltf
 	const loader = new GLTFLoader();
-
-	loader.load('/pc_monitor/scene.gltf', (data) => {
+	loader.load('/isometric_room/scene.gltf', (data) => {
 		const gltf = data;
 		object = gltf.scene;
-		object.scale.set(200.0, 200.0, 200.0);
-		object.position.set(0, -50, 0);
-		object.rotation.set(0, Math.PI / 2, 0);
-		// object.rotation.x = Math.PI / -2;
+		object.scale.set(10.0, 10.0, 10.0);
+		object.position.set(0, 0, 0);
+		object.rotation.y = THREE.MathUtils.degToRad(80); 
 		scene.add(object);
 		console.log(object);
 	});
-	//
+
+	const cssobj = document.getElementById("css3d")
+	const css3dobj = new CSS3DObject(cssobj)
+	css3dobj.position.set(-7.2, 9.38, -2.8);
+	css3dobj.rotation.set(0, THREE.MathUtils.degToRad(80), 0);
+	css3dobj.scale.set(0.015, 0.015, 0.015);
+	scene2.add(css3dobj);
 
 	renderer = new THREE.WebGLRenderer();
 	renderer.setPixelRatio(window.devicePixelRatio);
@@ -68,49 +54,40 @@ function init() {
 	document.body.appendChild(cssrender.domElement);
 
 	const controls = new OrbitControls( camera, cssrender.domElement );
-	controls.minZoom = 0.5;
-	controls.maxZoom = 2;
+	controls.enablezoom = false;
+
+	let wsw = window.screen.width;
+	if (wsw < 564) {
+		controls.minDistance = 80;
+		controls.maxDistance = 80;
+	}
+	else{
+		controls.minDistance = 40;
+		controls.maxDistance = 40;
+	}
+
+	controls.minPolarAngle = -Math.PI / 4;
+	controls.maxPolarAngle = Math.PI / 4;
+	controls.minAzimuthAngle = -Math.PI / 8;
+	controls.maxAzimuthAngle = Math.PI / 2;
 	
-
-	window.addEventListener('resize', onWindowResize);
-
+	window.addEventListener("DOMContentLoaded", onWindowResize);
+	window.addEventListener("resize", onWindowResize);
+	window.addEventListener("orientationchange", onWindowResize);
 }
 
 function onWindowResize() {
 
-	const aspect = window.innerWidth / window.innerHeight;
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
 
-	camera.left = - frustumSize * aspect / 2;
-	camera.right = frustumSize * aspect / 2;
-	camera.top = frustumSize / 2;
-	camera.bottom = - frustumSize / 2;
-
-	camera.updateProjectionMatrix();
-
-	renderer.setSize(window.innerWidth, window.innerHeight);
-
-	cssrender.setSize(window.innerWidth, window.innerHeight);
-
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    cssrender.setSize(window.innerWidth, window.innerHeight);
+	this.wsw = window.screen.width;
 }
 
 function animate() {
-
 	requestAnimationFrame(animate);
 	renderer.render(scene, camera);
 	cssrender.render(scene2, camera);
-
-}
-
-function updateCameraViewOffset({ fullWidth, fullHeight, x, y, width, height }) {
-
-	if (!camera.view) {
-
-		camera.setViewOffset(fullWidth || window.innerWidth, fullHeight || window.innerHeight, x || 0, y || 0, width || window.innerWidth, height || window.innerHeight);
-
-	} else {
-
-		camera.setViewOffset(fullWidth || camera.view.fullWidth, fullHeight || camera.view.fullHeight, x || camera.view.offsetX, y || camera.view.offsetY, width || camera.view.width, height || camera.view.height);
-
-	}
-
 }
